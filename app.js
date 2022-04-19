a303a37a46cc309b84d40a5b2c2782eba4ac043f
Olesya <asa88.88@bk.ru>
2022-04-19 11:58:18 +0300

    in work

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var swiper = new Swiper(".mySwiper", {
-   cssMode: true,
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
-   pagination: {
-      el: ".swiper-pagination",
-   },
-   mousewheel: true,
-   keyboard: true,
-});
+var btn_prev = document.querySelector('#gallery . buttons .prev');
+var btn_next = document.querySelector('#gallery . buttons .next');
+
+var cards = document.querySelectorAll('#gallery .card');
+var i = 0
+
+btn_prev.onclick = function () {
+   cards[i].style.display = 'none';
+   i--;
+
+   if (i >= cards.length) {
+      i = 0;
+   }
+
+   cards[i].style.display = 'block';
+}
+
+btn_next.onclick = function () {
+   cards[i].style.display = 'none';
+   i++;
+
+   if (i >= cards.length) {
+      i = 0;
+   }
+
+   cards[i].style.display = 'block';
+}
